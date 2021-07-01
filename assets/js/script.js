11cb373828fd68f7167f5028b95bdd9d58606e6f
tloveday <thomas_loveday848@hotmail.com>
2021-07-01 17:53:35 +0000

    Update to the script.js following meeting  with mentor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Credit for getting the game running and working: PortExe, https://www.youtube.com/watch?v=3uuQ3g92oPQ&list=PL4q8dcAYPWjTRKitqOzyyAU2jkRblHTFc&index=6&t=1601s
-
+// Credit is given to my mentor Antionio for guidance throughout the document clarifying problems, conflicts.
 //variables
 let flippedCard = false;
 let card1, card2;
-
+var time = 100;
+var timer = null;
 //The Game will wait fo the Dom to complete loading before running
 
 document.addEventListener("DOMContentLoaded", () =>{
+    startTimer();
 
     //Cards In Memory Game
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -16,22 +18,47 @@ document.addEventListener("DOMContentLoaded", () =>{
       card.addEventListener('click', cardFlip);
     })
 
+    //Check for Impostor- Win Condition.
+    function check_if_impostor(clicked_char){
+        return setTimeout(function (){
+            if (clicked_char === 'impostor'){
+                alert('You won');
+                clearTimeout(timer);
+            }
+            else{
+                console.log('I clicked ', clicked_char);
+            }
+        }, 500);    
+    }
+
+
+
     //CardFlip Function
     function cardFlip(){
       this.classList.add('visible');
       //Votes climb with each click
       voted();
-
-      //Give clicks identities to be checked
+      //Cards Identitied made via the data attribute
+      clicked_char = this.getAttribute('data-char');
+      check_if_impostor(clicked_char);
+        //Give clicks identities to be checked
         if(!flippedCard){
             flippedCard = true;
             card1 = this;
         }else{ 
-        card2 = this;
-        flippedCard = false;
-
-        //Check for match
-        checkMatch();
+            card2 = this;
+            flippedCard = false;
+            //Check for match
+            if (checkMatch()) {
+                console.log('We should lock the cards');
+                //stop the cards being repicked- once flipped.
+                card1.removeEventListener('click', cardFlip, false);
+                card2.removeEventListener('click', cardFlip, false);
+            }
+            else{
+                console.log('We should unflip the cards');
+                cardMisMatch();
+            }
         } 
       
     }
@@ -47,33 +74,34 @@ document.addEventListener("DOMContentLoaded", () =>{
     };
 
     //Timer
-    var time = 100
-    var timer = setInterval(countDown, 1000)
+    function startTimer() {
+        timer = setInterval(countDown, 1000);
+    }
     
+ 
+    // CountDown Timer   
     function countDown(){
         time=time-1;
     
-    if (time <= 0){
-     clearInterval(timer);
-     return;
-    }document.getElementById("time-remaining").innerHTML= time;
+        if (time <= 0){
+            clearInterval(timer);
+            return;
+        }
+        document.getElementById("time-remaining").innerHTML= time;
     }
 
     //Get Card Information
-    function getCardData(card){
-        return card.getElementsByClassName('card-data')[0].src
-    }
+//    function getCardData(card){
+//        return card.getElementsByClassName('card-data')[0].src
+ //   }
 
     //Check Match
     function checkMatch(){
-
-    card1Data = getCardData(card1)
-    card2Data = getCardData(card2)
-    
-    if (card1Data === card2Data){
-            //cardMatch();
+        if (card1.getAttribute('data-char') === card2.getAttribute('data-char')){
+            return true;
         }
-        else{cardMisMatch()
+        else{
+            false;
         }
     }
     //card Match
@@ -84,5 +112,5 @@ document.addEventListener("DOMContentLoaded", () =>{
         setTimeout(()=>{
             card1.classList.remove('visible');
             card2.classList.remove('visible');
-        }, 1000)
+        }, 500)
     }
